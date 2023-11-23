b6c8bf97ab5b441f246d1d7986e599845119143a
FrameworkTV <ioanejaniashvili19@gmail.com>
2023-11-23 20:54:43 +0400

    Add project files.

---
+++ vano - Task#1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace vano___Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your First Name");
+            var firstName = (Console.ReadLine());
+
+            Console.Write("Write your Last Name");
+            var lastName = (Console.ReadLine());
+
+            Console.WriteLine("Write your age");
+            var age = (Console.ReadLine());
+
+            Console.WriteLine("Write your Gender");
+            var gender = (Console.ReadLine());
+
+            var fullName = "Hello, my Full name is:" + firstName + " " + lastName;
+            Console.WriteLine(fullName);
+
+            var about = $"my age is: {age}, my gender is: {gender} ";
+            Console.WriteLine(about);
+        }
+    }
+}
